196900c6d3eefcf4cb744af32a0926a8016ef4e4
Khushi044 <khushi.saraswat_cs.h24@gla.ac.in>
2024-11-28 00:49:23 +0530

    string_q10.py

---
+++ coding-assignment/string/string_q10.py
@@ -0,0 +1,9 @@
+Format a string using placeholders
+Question: Format "Hello, [name]" with the name "Alice".
+Solution:
+
+python
+Copy code
+name = "Alice"
+formatted_s = f"Hello, {name}"
+print(formatted_s)  # Output: "Hello, Alice"
