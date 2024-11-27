d3ff69f27fa866e2f4238b186193a40e21810a59
Khushi044 <khushi.saraswat_cs.h24@gla.ac.in>
2024-11-28 00:43:58 +0530

    string_q03.py

---
+++ coding-assignment/string/string_q03.py
@@ -0,0 +1,9 @@
+Split a string
+Question: Split "apple,banana,grape" by commas.
+Solution:
+
+python
+Copy code
+s = "apple,banana,grape"
+split_s = s.split(",")
+print(split_s)  # Output: ['apple', 'banana', 'grape']
