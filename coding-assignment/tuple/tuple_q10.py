06f4adc10d259d935ef7d13ac7795f5082277678
Khushi044 <khushi.saraswat_cs.h24@gla.ac.in>
2024-11-28 00:34:19 +0530

    tuple_q10.py

---
+++ coding-assignment/tuple/tuple_q10.py
@@ -0,0 +1,8 @@
+Problem: Find the maximum and minimum values in t = (1, 5, 3, 9, 2).
+Solution:
+
+t = (1, 5, 3, 9, 2)
+maximum = max(t)
+minimum = min(t)
+print("Max:", maximum)
+print("Min:", minimum)
