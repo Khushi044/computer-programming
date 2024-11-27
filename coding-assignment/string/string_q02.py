c74ff3fb27b8330d0b3900fd15c090cd43d7f1f4
Khushi044 <khushi.saraswat_cs.h24@gla.ac.in>
2024-11-28 00:42:39 +0530

    string_q02.py

---
+++ coding-assignment/string/string_q02.py
@@ -0,0 +1,9 @@
+Replace a substring
+Question: Replace "cat" with "dog" in "The cat is cute.".
+Solution:
+
+python
+Copy code
+s = "The cat is cute."
+new_s = s.replace("cat", "dog")
+print(new_s)  # Output: "The dog is cute."
