01f8cb6f81d0c8c014630619ab0985a75c0e3b33
Khushi044 <khushi.saraswat_cs.h24@gla.ac.in>
2024-11-28 00:41:57 +0530

    string_q01.py

---
+++ coding-assignment/string/string_q01.py
@@ -0,0 +1,8 @@
+Find a substring in a string
+Question: Check if "learn" is in "I love to learn Python.".
+Solution:
+
+python
+Copy code
+s = "I love to learn Python."
+print("learn" in s)  # Output: True
