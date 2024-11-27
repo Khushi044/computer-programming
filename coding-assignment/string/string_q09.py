466729f07a62fd9cb01e5c35613f81a57794cdf6
Khushi044 <khushi.saraswat_cs.h24@gla.ac.in>
2024-11-28 00:48:43 +0530

    string_q09.py

---
+++ coding-assignment/string/string_q09.py
@@ -0,0 +1,9 @@
+Remove a specific character
+Question: Remove all "a"s from "banana".
+Solution:
+
+python
+Copy code
+s = "banana"
+removed_a = s.replace("a", "")
+print(removed_a)  # Output: "bnn"
