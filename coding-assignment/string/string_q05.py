1ca7e4885e47256d1d2fb08b7119627348cab626
Khushi044 <khushi.saraswat_cs.h24@gla.ac.in>
2024-11-28 00:45:47 +0530

    string_q05.py

---
+++ coding-assignment/string/string_q05.py
@@ -0,0 +1,15 @@
+ Iterate over each character in a string
+Question: Print each character of "hello".
+Solution:
+
+python
+Copy code
+s = "hello"
+for char in s:
+    print(char)
+# Output:
+# h
+# e
+# l
+# l
+# o
