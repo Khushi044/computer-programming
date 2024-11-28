5da4e6aaa51a2bc704f9ea4010d416c376b00094
Khushi044 <khushi.saraswat_cs.h24@gla.ac.in>
2024-11-28 14:36:19 +0530

    list_q11.py

---
+++ coding-assignment/list/list_q11.py
@@ -0,0 +1,9 @@
+Sort a list in descending order
+Question: Sort [4, 1, 3, 2] in descending order.
+Solution:
+
+python
+Copy code
+lst = [4, 1, 3, 2]
+lst.sort(reverse=True)
+print(lst)  # Output: [4, 3, 2, 1]
