e5ec18dd80a599fb3f323c0a35e596782f63b111
Khushi044 <khushi.saraswat_cs.h24@gla.ac.in>
2024-11-28 09:02:46 +0530

    dict_q10.py

---
+++ coding-assignment/dict/dict_q10.py
@@ -0,0 +1,12 @@
+Problem Statement:
+Given a dictionary, check whether it is empty or not.
+
+Solution:
+my_dict = {}
+
+if not my_dict:
+    print("Dictionary is empty")
+else:
+    print("Dictionary is not empty")
+Output:
+Dictionary is empty
