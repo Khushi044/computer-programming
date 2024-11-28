ce4a7630742eb0c40fe80d03cfcd9635b654a7d6
Khushi044 <khushi.saraswat_cs.h24@gla.ac.in>
2024-11-28 11:15:24 +0530

    function_q10.py

---
+++ coding-assignment/function/function_q10.py
@@ -0,0 +1,6 @@
+Problem: Write a function that converts Celsius to Fahrenheit.
+
+Solution:
+def celsius_to_fahrenheit(c):
+    return (c * 9/5) + 32
+print(celsius_to_fahrenheit(0))  # Output: 32.0
