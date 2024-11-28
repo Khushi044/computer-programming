5f3817ced1f2272637409fa976e9b2961e9553d2
Khushi044 <khushi.saraswat_cs.h24@gla.ac.in>
2024-11-28 11:18:30 +0530

    function_q14.py

---
+++ coding-assignment/function/function_q14.py
@@ -0,0 +1,8 @@
+Problem: Write a function that removes all spaces from a string.
+
+Solution:
+def remove_spaces(s):
+    return s.replace(" ", "")
+
+print(remove_spaces("hello world"))  # Output: "helloworld"
+
