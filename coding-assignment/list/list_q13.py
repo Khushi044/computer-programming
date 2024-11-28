0a543d7469601653781793bc1fcc3ec994dc8fd3
Khushi044 <khushi.saraswat_cs.h24@gla.ac.in>
2024-11-28 14:38:19 +0530

    list_q13.py

---
+++ coding-assignment/list/list_q13.py
@@ -0,0 +1,6 @@
+Create a list of squares using a loop
+Question: Create a list of squares of [1, 2, 3, 4].
+Solution:
+lst = [1, 2, 3, 4]
+squares = [x**2 for x in lst]
+print(squares)  # Output: [1, 4, 9, 16]
